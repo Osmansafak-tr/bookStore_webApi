b8bcc641f293b1819a1a391eea34ce5096b9cad7
Osmansafak-tr <osmansfk53@gmail.com>
2023-06-01 18:21:56 +0300

    author entity created

---
+++ WebApi/Entities/Authors.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Entities
+{
+    public class Authors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
